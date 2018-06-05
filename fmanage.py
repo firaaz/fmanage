9af2d229870dc5d811528cff779df059a5037942
Firaaz Farook <firaazfarook19@gmail.com>
2018-06-06 00:34:02 +0530

    Created the file in the home dir

--- fmanage.py
+++ fmanage.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python
+
+import argparse
+import os
+
+home = '/mnt/DATA/'
+os.chdir(home)
+os.system('mkdir home')
+dirc = home + 'home'
+os.chdir(dirc)
+os.system('mkdir ' + ' '.join(['Documents', 'Music', 'Movies', 'Pictures', 'Torrents']))
+# for x in os.listdir(home):
+#    if 'Downloads' == x:
+#        dirc = home + 'Downloads'
+#        os.chdir(dirc)
+#
+#        print(os.getcwd())
+#        print(os.listdir())
