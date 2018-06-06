d5343262facf4e76eb3867cfd27b7a4a03c9b113
Firaaz Farook <firaazfarook19@gmail.com>
2018-06-06 06:36:28 +0530

    updated filetype connections

--- fmanage.py
+++ fmanage.py
@@ -3,10 +3,19 @@
 import argparse
 import os
 
+file_types = dict()
 home = '/mnt/DATA/'
-files = ['Documents', 'Music', 'Movies', 'Pictures', 'Torrents', 'Downloads']
+files = ['Documents', 'Music', 'Pictures', 'Torrents', 'Downloads', 'Video']
 # home = os.environ['HOME']
 dirc = home + 'home'
+
+# File type bindings
+file_types['Video'] = ['webm', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'ogg', 'mp4', 'm4v']
+file_types['Music'] = ['mp3', 'mpa', 'aac', 'oga', 'ape', 'm4a', 'flac', 'm4p', 'wav']
+file_types['Pictures'] = ['jpeg', 'jfif', 'exif', 'gif', 'bmp', 'png']
+file_types['Documents'] = []
+file_types['Movies'] = []
+
 
 os.chdir(home)
 if 'home' not in os.listdir(home):
