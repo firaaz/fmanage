9df4aae9b36e919b67556fdb17032e6a00aceb52
Firaaz Farook <firaazfarook19@gmail.com>
2018-06-06 06:04:55 +0530

    better file creation

--- fmanage.py
+++ fmanage.py
@@ -4,15 +4,19 @@ import argparse
 import os
 
 home = '/mnt/DATA/'
-os.chdir(home)
-os.system('mkdir home')
+files = ['Documents', 'Music', 'Movies', 'Pictures', 'Torrents', 'Downloads']
+# home = os.environ['HOME']
 dirc = home + 'home'
+
+os.chdir(home)
+if 'home' not in os.listdir(home):
+    os.system('mkdir home')
 os.chdir(dirc)
-os.system('mkdir ' + ' '.join(['Documents', 'Music', 'Movies', 'Pictures', 'Torrents']))
-# for x in os.listdir(home):
-#    if 'Downloads' == x:
-#        dirc = home + 'Downloads'
-#        os.chdir(dirc)
-#
-#        print(os.getcwd())
-#        print(os.listdir())
+
+# creating the required files if it isnt already created
+for file_name in files:
+    if file_name not in os.listdir(os.getcwd()):
+        os.makedirs(file_name)
+
+for name, subdir, filename in os.walk(os.getcwd(), topdown=False):
+    print(filename)
